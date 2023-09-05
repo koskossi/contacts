32a56e203070bd5f7b745703f911e7c49355904b
mohamed <mohamed.koskossi@gmail.com>
2023-09-05 10:48:14 +0200

    Revue de code home component

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef, OnChanges, DoCheck} from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef} from '@angular/core';
 import {ActivatedRoute, Data, ParamMap, Router, RouterModule} from '@angular/router';
 import {combineLatest, filter, Observable, Subject, switchMap, tap} from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -29,33 +29,34 @@ import {MatSort} from "@angular/material/sort";
   imports: [SharedModule, RouterModule, MatButtonModule],
 })
 export default class HomeComponent implements OnInit, OnDestroy {
-  account: Account | null = null;
-  private readonly destroy$ = new Subject<void>();
 
+  // Contact
   contacts?: IContact[];
-  isLoading = false;
-  predicate = 'id';
-  ascending = true;
-  filters: IFilterOptions = new FilterOptions();
-
-  itemsPerPage = ITEMS_PER_PAGE;
-  totalItems = 0;
-  page = 1;
-
+  exportList: UserContact[] =[];
+  account: Account | null = null;
 
   /// Table material
   columnsToDisplay: string[] =[];
   dataSource: MatTableDataSource<UserContact>;
   @ViewChild(MatSort) sort: MatSort = new MatSort();
   @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);
-  exportList: UserContact[] =[];
+  isLoading = false;
+  predicate = 'id';
+  ascending = true;
+  filters: IFilterOptions = new FilterOptions();
+  itemsPerPage = ITEMS_PER_PAGE;
+  totalItems = 0;
+  page = 1;
 
+  // Observable
+  private readonly destroy$ = new Subject<void>();
 
   constructor(private accountService: AccountService, private router: Router,
               protected contactService: ContactService,
               protected activatedRoute: ActivatedRoute,
               protected modalService: NgbModal
               ) {
+    // Initialisatuion de l'entete de la table contact
     this.columnsToDisplay =  ['nom','prenom', 'age','address','action']
     let listeContact: Array<UserContact> = new Array<UserContact>();
     this.dataSource = new MatTableDataSource(listeContact);
@@ -72,7 +73,7 @@ export default class HomeComponent implements OnInit, OnDestroy {
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
       .subscribe(account => (this.account = account));
-    // récpérer la liste des contacts
+    // récupérer la liste des contacts
     this.recupererContacts();
     this.filters.filterChanges.subscribe(filterOptions => this.handleNavigation(1, this.predicate, this.ascending, filterOptions));
   }
@@ -84,34 +85,54 @@ export default class HomeComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
+  /**
+   * Recherche des contacts dans la table de données en fonction de la valeur saisie.
+   * @param event L'événement déclenché lors de la saisie dans la barre de recherche.
+   */
   rechercher(event: Event) {
+    // Récupérer la valeur saisie dans la barre de recherche
     const target = event.target as HTMLButtonElement;
     this.dataSource.filter = target.value.trim().toLowerCase();
-    console.log(this.dataSource.filter);
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
   }
 
-
+  /**
+   * Supprime un contact après confirmation de l'utilisateur en ouvrant une boîte de dialogue de confirmation.
+   * @param contact Le contact à supprimer.
+   */
   delete(contact: IContact): void {
+    // Ouvre une boîte de dialogue de confirmation de suppression avec les détails du contact.
     const modalRef = this.modalService.open(ContactDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
     modalRef.componentInstance.contact = contact;
-    // unsubscribe not needed because closed completes on modal close
+    // Écoute la fermeture de la boîte de dialogue (complétion de modalRef.closed).
     modalRef.closed
       .pipe(
+        // Filtrer seulement les raisons de fermeture équivalentes à l'événement ITEM_DELETED_EVENT.
         filter(reason => reason === ITEM_DELETED_EVENT),
+        // Lorsque l'élément est supprimé, effectuer une action.
         switchMap(() => this.loadFromBackendWithRouteInformations())
       )
       .subscribe({
+        // Lorsque la réponse est réussie, exécutez cette fonction de rappel.
         next: (res: EntityArrayResponseType) => {
           this.onResponseSuccess(res);
         },
       });
   }
 
+  /**
+   * Récupère les contacts depuis le backend en utilisant les informations de route.
+   */
   recupererContacts(): void {
+    // Appelle la méthode loadFromBackendWithRouteInformations() et souscrit à son observable.
     this.loadFromBackendWithRouteInformations().subscribe({
+      /**
+       * Gère la réponse du backend en cas de succès.
+       *
+       * @param res - La réponse du backend contenant les contacts.
+       */
       next: (res: EntityArrayResponseType) => {
         this.onResponseSuccess(res);
       },
@@ -165,22 +186,39 @@ export default class HomeComponent implements OnInit, OnDestroy {
     this.totalItems = Number(headers.get(TOTAL_COUNT_RESPONSE_HEADER));
   }
 
+  /**
+   * Effectue une requête au backend pour récupérer une liste d'entités paginées.
+   *
+   * @param page - La page de résultats à récupérer (facultatif, par défaut 1).
+   * @param predicate - Le prédicat de tri pour la requête (facultatif).
+   * @param ascending - Indique si le tri est ascendant (true) ou descendant (false) (facultatif).
+   * @param filterOptions - Un tableau d'options de filtrage pour la requête (facultatif).
+   * @returns Un observable contenant la réponse du backend.
+   * @protected
+   */
   protected queryBackend(
     page?: number,
     predicate?: string,
     ascending?: boolean,
     filterOptions?: IFilterOption[]
   ): Observable<EntityArrayResponseType> {
+    // Active le drapeau isLoading pour indiquer le chargement en cours.
     this.isLoading = true;
     const pageToLoad: number = page ?? 1;
+
+    // Construit l'objet de requête avec les paramètres fournis.
     const queryObject: any = {
       page: pageToLoad - 1,
       size: this.itemsPerPage,
       sort: this.getSortQueryParam(predicate, ascending),
     };
+
+    // Ajoute les options de filtrage à l'objet de requête si elles sont spécifiées.
     filterOptions?.forEach(filterOption => {
       queryObject[filterOption.name] = filterOption.values;
     });
+
+    // Appelle le service de contact pour effectuer la requête et retourne l'observable.
     return this.contactService.query(queryObject).pipe(tap(() => (this.isLoading = false)));
   }
 
@@ -210,14 +248,26 @@ export default class HomeComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Cette méthode exporte les données au format CSV à partir d'une liste de données.
+   */
   exportCsv() {
+    // Convertit la liste de données en une chaîne CSV.
     const csvData = this.convertToCSV(this.exportList);
+
+    // Crée un objet Blob à partir de la chaîne CSV avec le type 'text/csv'.
     const blob = new Blob([csvData], { type: 'text/csv' });
+    // Crée une URL pour le Blob.
     const url = window.URL.createObjectURL(blob);
+
+    // Crée un élément de lien (a) pour télécharger le fichier CSV.
     const button = document.createElement('a');
+
     button.href = url;
     button.download = 'exported-data.csv';
     document.body.appendChild(button);
+
+    // Clique sur le bouton pour déclencher le téléchargement.
     button.click();
     window.URL.revokeObjectURL(url);
   }
@@ -226,8 +276,6 @@ export default class HomeComponent implements OnInit, OnDestroy {
     const rows = data.map(item => Object.values(item).join(','));
     return `${header}\n${rows.join('\n')}`;
   }
-
-  // trackId = (_index: number, item: IContact): number => this.contactService.getContactIdentifier(item);
 
 
 }
